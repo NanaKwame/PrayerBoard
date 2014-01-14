6d4e2d950d3cfcd033d201e8449e8128ac423977
NanaKwame <easj.asante@gmail.com>
2014-01-14 13:04:48 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PrayerBoard::Application.config.secret_key_base = '32dd92e4f35a53cc53c3d72f11f8c4b9eacfdb24d788641146e5e3c819e549b1c1db036b590a28ef4021ee831cc642c56004194d7e21296216c48f6b39a8874e'
