691d7cd36d8f772392ec10a7bffc8f6a282fd816
NanaKwame <easj.asante@gmail.com>
2015-09-13 16:12:20 -0700

    setting app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141230181255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,7 +105,6 @@ ActiveRecord::Schema.define(version: 0) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
->>>>>>> models_setup
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -131,7 +130,6 @@ ActiveRecord::Schema.define(version: 0) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
->>>>>>> login_user
 
   create_table "verses", force: true do |t|
     t.integer  "bible_verse_id"
